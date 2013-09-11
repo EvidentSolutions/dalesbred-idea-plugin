77f18bd88e515ccd5795fd4dd791bb755e97947d
Juha Komulainen <juha.komulainen@evident.fi>
2013-09-11 14:02:39 +0300

    Simple findAll-annotation.

--- src/fi/evident/dalesbred/plugin/idea/DalesbredAnnotator.java
+++ src/fi/evident/dalesbred/plugin/idea/DalesbredAnnotator.java
@@ -2,21 +2,25 @@ package fi.evident.dalesbred.plugin.idea;
 
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
+import com.intellij.patterns.PsiExpressionPattern;
+import com.intellij.patterns.PsiMethodPattern;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiLiteralExpression;
+import com.intellij.psi.PsiExpression;
 import org.jetbrains.annotations.NotNull;
+
+import static com.intellij.patterns.PsiJavaPatterns.psiExpression;
+import static com.intellij.patterns.PsiJavaPatterns.psiMethod;
 
 public class DalesbredAnnotator implements Annotator {
 
     @Override
-    public void annotate(@NotNull PsiElement psiElement, @NotNull AnnotationHolder annotationHolder) {
-        if (psiElement instanceof PsiLiteralExpression) {
-            PsiLiteralExpression literal = (PsiLiteralExpression) psiElement;
+    public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
+        PsiMethodPattern method = psiMethod().definedInClass("fi.evident.dalesbred.Database").withName("findAll");
 
-            String value = (String) literal.getValue();
-            if (value != null && value.startsWith("dalesbred:")) {
-                annotationHolder.createErrorAnnotation(psiElement, "dalesbred!");
-            }
+        PsiExpressionPattern.Capture<PsiExpression> firstParameter = psiExpression().methodCallParameter(0, method);
+
+        if (firstParameter.accepts(element)) {
+            holder.createErrorAnnotation(element, "foo");
         }
     }
 }
