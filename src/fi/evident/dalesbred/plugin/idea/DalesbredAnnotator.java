8a828646681a9481ade175282bcc0803f53830d9
Juha Komulainen <juha.komulainen@evident.fi>
2013-09-11 22:28:36 +0300

    Simple inspection for interface-parameters as class-type.

--- src/fi/evident/dalesbred/plugin/idea/DalesbredAnnotator.java
+++ src/fi/evident/dalesbred/plugin/idea/DalesbredAnnotator.java
@@ -2,25 +2,45 @@ package fi.evident.dalesbred.plugin.idea;
 
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
+import com.intellij.openapi.util.Key;
 import com.intellij.patterns.PsiExpressionPattern;
 import com.intellij.patterns.PsiMethodPattern;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiExpression;
+import com.intellij.psi.*;
+import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import static com.intellij.patterns.PsiJavaPatterns.psiExpression;
-import static com.intellij.patterns.PsiJavaPatterns.psiMethod;
+import static com.intellij.patterns.PsiJavaPatterns.*;
 
 public class DalesbredAnnotator implements Annotator {
 
     @Override
     public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
-        PsiMethodPattern method = psiMethod().definedInClass("fi.evident.dalesbred.Database").withName("findAll");
+        // TODO: handle key and value of findMap
+        PsiMethodPattern method = psiMethod().definedInClass("fi.evident.dalesbred.Database").withName(string().oneOf("findUnique", "findAll", "findUniqueOrNull"));
 
-        PsiExpressionPattern.Capture<PsiExpression> firstParameter = psiExpression().methodCallParameter(0, method);
+        Key<PsiExpression> exp = Key.create("classObject");
+        PsiExpressionPattern.Capture<PsiExpression> firstParameter =
+                psiExpression().methodCallParameter(0, method).save(exp);
 
-        if (firstParameter.accepts(element)) {
-            holder.createErrorAnnotation(element, "foo");
+        ProcessingContext ctx = new ProcessingContext();
+        if (firstParameter.accepts(element, ctx)) {
+            PsiClass cl = resolveType(ctx.get(exp));
+            if (cl != null && cl.isInterface()) {
+                // TODO: allow interfaces of known (and registered) types
+                holder.createErrorAnnotation(element, "Class may not refer to an interface.");
+            }
         }
+    }
+
+    @Nullable
+    private static PsiClass resolveType(@NotNull PsiExpression value) {
+        if (value instanceof PsiClassObjectAccessExpression) {
+            PsiType type = ((PsiClassObjectAccessExpression) value).getOperand().getType();
+            if (type instanceof PsiClassType)
+                return ((PsiClassType) type).resolve();
+        }
+
+        return null;
     }
 }
